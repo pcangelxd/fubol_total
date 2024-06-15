7273ca202d3e69f8267c161c7b7336a10b2e2930
pcangelxd <70732983+pcangelxd@users.noreply.github.com>
2024-06-15 16:11:25 -0500

    Update util.py

--- conmebol/util.py
+++ conmebol/util.py
@@ -1,3 +1,7 @@
-API_MATCHES='https://onefootball.com/es/competicion/conmebol-eliminatorias-copa-mundial-74/partidos'
-API_RESULTS='https://onefootball.com/es/competicion/conmebol-eliminatorias-copa-mundial-74/resultados'
-API_CLASSIFICATION='https://onefootball.com/es/competicion/conmebol-eliminatorias-copa-mundial-74/clasificacion'
+API_MATCHES = 'https://onefootball.com/es/competicion/conmebol-eliminatorias-copa-mundial-74/partidos'
+API_RESULTS_CONMEBOL = 'https://onefootball.com/es/competicion/conmebol-eliminatorias-copa-mundial-74/resultados'
+API_CLASSIFICATION = 'https://onefootball.com/es/competicion/conmebol-eliminatorias-copa-mundial-74/clasificacion'
+
+API_RESULTS_LALIGA = 'https://onefootball.com/es/competicion/laliga-10/resultados'
+API_RESULTS_PREMIER_LEAGUE = 'https://onefootball.com/es/competicion/premier-league/resultados'
+# Añadir más URLs según sea necesario
