b1dcd575e0353d69d6965df99864c5023790b339
pcangelxd <70732983+pcangelxd@users.noreply.github.com>
2024-06-15 16:22:47 -0500

    Update results.py

--- conmebol/results.py
+++ conmebol/results.py
@@ -1,95 +1,44 @@
-import httpx
-from bs4 import BeautifulSoup
-from dataclasses import asdict
-from .models import LastMatches
-from .util import (
-    API_RESULTS_CONMEBOL,
-    API_RESULTS_LALIGA,
-    API_RESULTS_PREMIER_LEAGUE,
-    # Importar más URLs según sea necesario
-)
-import logging
+import json
+from flask import Flask, render_template, jsonify
+from flask_cors import CORS
+from exceptions import page_not_found, internal_server_error
+from conmebol import Classification, Results, Matches
 
-# Configurar el registro
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger(__name__)
+app = Flask(__name__)
+CORS(app)
 
-def get_match_statistics(_journeys: list, matches: BeautifulSoup):
+app.register_error_handler(404, page_not_found)
+app.register_error_handler(500, internal_server_error)
+
+def render_json(_object: dict):
+    response = app.response_class(
+        response=json.dumps(_object),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/api/classification')
+def classification():
     try:
-        team_names = [x.text for team in matches for x in team.find_all('span', 'SimpleMatchCardTeam_simpleMatchCardTeam__name__7Ud8D')]
-        goals = [x.text for team in matches for x in team.find_all('span', 'SimpleMatchCardTeam_simpleMatchCardTeam__score__UYMc_')]
-        match_dates = [x.find('time').get('datetime') for team in matches for x in team.find_all('div', 'SimpleMatchCard_simpleMatchCard__matchContent__prwTf')]
-
-        results = {}
-        date_index = 0
-        journey_index = 0
-
-        for i in range(0, len(team_names), 2):
-            if i + 1 >= len(team_names) or date_index >= len(match_dates):
-                break  # Evitar acceso fuera del rango
-
-            first_team = {'country': team_names[i], 'goals': goals[i] if i < len(goals) else '0'}
-            second_team = {'country': team_names[i + 1], 'goals': goals[i + 1] if i + 1 < len(goals) else '0'}
-            winner = 'Tie' if goals[i] == goals[i + 1] else team_names[i] if goals[i] > goals[i + 1] else team_names[i + 1]
-            date = match_dates[date_index]
-
-            data = LastMatches(
-                first_team=first_team,
-                second_team=second_team,
-                winner=winner,
-                date=date
-            )
-
-            if _journeys[journey_index] not in results:
-                results[_journeys[journey_index]] = []
-
-            results[_journeys[journey_index]].append(asdict(data))
-            date_index += 1
-
-            if (i + 2) % 10 == 0:
-                journey_index += 1
-
-        return results
-
+        return render_json(Classification().get_positions)
     except Exception as e:
-        logger.error(f"Error processing match statistics: {e}")
-        return {}
+        return jsonify({"error": str(e)}), 500
 
-class Results(object):
-    def __init__(self) -> None:
-        self.api_urls = [
-            API_RESULTS_CONMEBOL,
-            API_RESULTS_LALIGA,
-            API_RESULTS_PREMIER_LEAGUE,
-            # Añadir más URLs según sea necesario
-        ]
-        self.results = {}
+@app.route('/api/results')
+def results():
+    try:
+        return render_json(Results().get_results)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
-    def fetch_results(self, api_url):
-        try:
-            response = httpx.get(api_url, timeout=10.0)
-            response.raise_for_status()
-            soup = BeautifulSoup(response.content, "html.parser")
-            section_journeys = soup.find('div', 'MatchCardsListsAppender_container__y5ame')
-
-            _journeys = [journey.text for journey in section_journeys.find_all('div', 'SectionHeader_container__iVfZ9')]
-            _matches = section_journeys.find_all('div', 'SimpleMatchCard_simpleMatchCard__content__ZWt2p')
-
-            return get_match_statistics(_journeys, _matches)
-        except httpx.HTTPStatusError as e:
-            logger.error(f"HTTP error occurred: {e.response.status_code} - {e.response.text}")
-            return {}
-        except httpx.RequestError as e:
-            logger.error(f"Request error occurred: {e}")
-            return {}
-        except Exception as e:
-            logger.error(f"An unexpected error occurred: {e}")
-            return {}
-
-    @property
-    def get_results(self):
-        for api_url in self.api_urls:
-            league_results = self.fetch_results(api_url)
-            self.results.update(league_results)
-
-        return self.results
+@app.route('/api/matches')
+def matches():
+    try:
+        return render_json(Matches().get_matches)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
