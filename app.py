052d3af9be638cd9b0a36c4e3c05b315be0454c4
pcangelxd <70732983+pcangelxd@users.noreply.github.com>
2024-06-18 00:09:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,16 +3,22 @@ from flask import Flask, render_template, jsonify
 from flask_cors import CORS
 import httpx
 from bs4 import BeautifulSoup
+from dataclasses import asdict
 import logging
+from exceptions import page_not_found, internal_server_error
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
+API_RESULTS_CONMEBOL = 'https://onefootball.com/es/competicion/conmebol-eliminatorias-copa-mundial-74/resultados'
+API_RESULTS_LALIGA = 'https://onefootball.com/es/competicion/laliga-10/resultados'
+API_RESULTS_PREMIER_LEAGUE = 'https://onefootball.com/es/competicion/premier-league/resultados'
+
 app = Flask(__name__)
 CORS(app)
 
-app.register_error_handler(404, lambda e: (jsonify(error=str(e)), 404))
-app.register_error_handler(500, lambda e: (jsonify(error=str(e)), 500))
+app.register_error_handler(404, page_not_found)
+app.register_error_handler(500, internal_server_error)
 
 def render_json(_object: dict):
     response = app.response_class(
@@ -22,64 +28,24 @@ def render_json(_object: dict):
     )
     return response
 
-class ImageScraper:
-    def __init__(self, url):
-        self.url = url
-        self.images = {}
+class LastMatches:
+    def __init__(self, first_team, second_team, winner, date):
+        self.first_team = first_team
+        self.second_team = second_team
+        self.winner = winner
+        self.date = date
 
-    def fetch_images(self):
+    def to_dict(self):
+        return asdict(self)
+
+class Results:
+    def __init__(self, api_url):
+        self.api_url = api_url
+        self.results = {}
+
+    def fetch_results(self):
         try:
-            logger.debug(f"Fetching images from: {self.url}")
-            response = httpx.get(self.url, timeout=10.0)
+            response = httpx.get(self.api_url, timeout=10.0)
             response.raise_for_status()
             soup = BeautifulSoup(response.content, "html.parser")
-            
-            image_elements = soup.find_all('img')
-            
-            if not image_elements:
-                logger.warning(f"No images found for URL: {self.url}")
-                return {}
-
-            self.images = self.extract_images(image_elements)
-            return self.images
-        except httpx.HTTPStatusError as e:
-            logger.error(f"HTTP error occurred: {e.response.status_code} - {e.response.text}")
-            return {}
-        except httpx.RequestError as e:
-            logger.error(f"Request error occurred: {e}")
-            return {}
-        except Exception as e:
-            logger.error(f"An unexpected error occurred: {e}")
-            return {}
-
-    def extract_images(self, image_elements):
-        try:
-            images = {}
-            for index, img in enumerate(image_elements):
-                img_src = img.get('src')
-                if img_src and 'events/factor' in img_src:  # Filtrar imágenes específicas
-                    img_name = f"image_{index + 1}"
-                    images[img_name] = img_src
-                    logger.debug(f"Found image: {img_name} -> {img_src}")
-
-            return images
-        except Exception as e:
-            logger.error(f"Error extracting images: {e}")
-            return {}
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/api/images')
-def fetch_images():
-    try:
-        url = "https://lordsmobilecartograph.ru/Kingdom?K=959"
-        scraper = ImageScraper(url)
-        images = scraper.fetch_images()
-        return render_json(images)
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-
-if __name__ == '__main__':
-    app.run(debug=True)
+            section_journeys = soup.find('div', 'MatchCardsLists
